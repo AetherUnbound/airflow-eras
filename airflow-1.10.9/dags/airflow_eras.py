d8724b9df79a73b3bed19f5cf052683a0716ac1e
Madison Swain-Bowden <bowdenm@spu.edu>
2023-09-08 14:28:12 -0700

    Remove unnecessary import from 1.10

--- airflow-1.10.9/dags/airflow_eras.py
+++ airflow-1.10.9/dags/airflow_eras.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
