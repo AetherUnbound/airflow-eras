f2526dfe048370cc0d8f625590c3d0c907d62e78
Madison Swain-Bowden <bowdenm@spu.edu>
2023-09-07 17:02:23 -0700

    Add the rest of the 1.7 DAG code

--- airflow-1.7/dags/airflow_eras.py
+++ airflow-1.7/dags/airflow_eras.py
@@ -1,22 +1,26 @@
+from __future__ import print_function
+
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
 from datetime import datetime, timedelta
+
+import funcs
 
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2015, 6, 1),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-    'catchup': False,
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2015, 6, 1),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    "catchup": False,
 }
 
-dag = DAG('airflow_eras', default_args=default_args, schedule_interval="@daily")
+dag = DAG("airflow_eras", default_args=default_args, schedule_interval=None)
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
 # t1 = BashOperator(
@@ -24,27 +28,68 @@ dag = DAG('airflow_eras', default_args=default_args, schedule_interval="@daily")
 #     bash_command='date',
 #     dag=dag)
 
+# Variable (then conf) as seed
+# Branch based on odd or even?
+# Generate 5 random numbers using seed
+# Each task does some "process" on them (fail if % 3?)
+# Gather all the numbers using DummyOperator and flow control
+# Report available numbers
+
+get_seed = PythonOperator(
+    task_id="get_seed",
+    python_callable=funcs.determine_seed,
+    provide_context=True,
+    dag=dag,
+)
+
+determine_continue = BranchPythonOperator(
+    task_id="should_continue",
+    python_callable=funcs.should_continue,
+    provide_context=True,
+    dag=dag,
+)
+
+workflow_stopped = PythonOperator(
+    task_id="do_not_continue",
+    python_callable=print,
+    op_args=["Workflow has been skipped!"],
+    dag=dag,
+)
+
+generate = PythonOperator(
+    task_id="generate_numbers",
+    python_callable=funcs.generate_numbers,
+    provide_context=True,
+    dag=dag,
+)
+
+gather = DummyOperator(
+    task_id="gather_numbers",
+    trigger_rule="all_done",
+    dag=dag,
+)
+
+for index in range(funcs.TASK_COUNT):
+    process = PythonOperator(
+        task_id="process_number_{}".format(index),
+        python_callable=funcs.process_number,
+        provide_context=True,
+        op_kwargs={"index": index},
+        dag=dag,
+        retries=0,
+    )
+    generate.set_downstream(process)
+    process.set_downstream(gather)
+
+report = PythonOperator(
+    task_id="report_numbers",
+    python_callable=funcs.report_numbers,
+    provide_context=True,
+    dag=dag,
+)
 
 
-t2 = BashOperator(
-    task_id='sleep',
-    bash_command='sleep 5',
-    retries=3,
-    dag=dag)
-
-templated_command = """
-    {% for i in range(5) %}
-        echo "{{ ds }}"
-        echo "{{ macros.ds_add(ds, 7)}}"
-        echo "{{ params.my_param }}"
-    {% endfor %}
-"""
-
-t3 = BashOperator(
-    task_id='templated',
-    bash_command=templated_command,
-    params={'my_param': 'Parameter I passed in'},
-    dag=dag)
-
-t2.set_upstream(t1)
-t3.set_upstream(t1)
+get_seed.set_downstream(determine_continue)
+determine_continue.set_downstream(workflow_stopped)
+determine_continue.set_downstream(generate)
+gather.set_downstream(report)
---
+++ airflow-1.7/dags/funcs.py
@@ -0,0 +1,47 @@
+import logging
+import random
+import time
+
+from airflow.models import Variable
+
+
+TASK_COUNT = 5
+
+log = logging.getLogger(__name__)
+
+
+def determine_seed(**context):
+    # First get from Variables
+    seed = Variable.get("seed", deserialize_json=True, default_var=0)
+    # Override with context if provided, defaulting to current time if not
+    seed = context["dag_run"].conf.get("seed", seed or int(time.time()))
+    return seed
+
+
+def should_continue(**context):
+    seed = context["ti"].xcom_pull("get_seed")
+    if seed % 2 == 0:
+        return "do_not_continue"
+    return "generate_numbers"
+
+
+def generate_numbers(**context):
+    seed = context["ti"].xcom_pull("get_seed")
+    random.seed(seed)
+    numbers = [random.randrange(100) for _ in range(TASK_COUNT)]
+    return numbers
+
+
+def process_number(index, **context):
+    number = context["ti"].xcom_pull("generate_numbers")[index]
+    if number % 3 == 0:
+        raise ValueError("Cannot process value: {}".format(number))
+    return number**2
+
+
+def report_numbers(**context):
+    total = 0
+    for index in range(TASK_COUNT):
+        number = context["ti"].xcom_pull("process_number_{}".format(index)) or 0
+        total += number
+    log.info("Total value computed: {}".format(total))
