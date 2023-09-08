f2cb4411a6103c098dadf7808bc0ebff4b736029
Madison Swain-Bowden <bowdenm@spu.edu>
2023-09-08 12:08:42 -0700

    Add 1.10.9 DAGs

---
+++ airflow-1.10.9/dags/airflow_eras.py
@@ -0,0 +1,81 @@
+from __future__ import print_function
+
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
+from datetime import datetime, timedelta
+
+import funcs
+
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2015, 6, 1),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+
+XCOM_PULL = "{{ ti.xcom_pull('%s') }}"
+
+
+with DAG("airflow_eras", default_args=default_args, schedule_interval=None) as dag:
+    get_seed = PythonOperator(
+        task_id="get_seed",
+        python_callable=funcs.determine_seed,
+        op_kwargs={
+            "conf": "{{ dag_run.conf or {} }}",
+        },
+    )
+
+    determine_continue = BranchPythonOperator(
+        task_id="should_continue",
+        python_callable=funcs.should_continue,
+        op_kwargs={
+            "seed": XCOM_PULL % get_seed.task_id,
+        },
+    )
+
+    workflow_stopped = PythonOperator(
+        task_id="do_not_continue",
+        python_callable=print,
+        op_args=["Workflow has been skipped!"],
+    )
+
+    generate = PythonOperator(
+        task_id="generate_numbers",
+        python_callable=funcs.generate_numbers,
+        op_kwargs={
+            "seed": XCOM_PULL % get_seed.task_id,
+        },
+    )
+
+    gather = DummyOperator(
+        task_id="gather_numbers",
+        trigger_rule="none_skipped",
+    )
+
+    for index in range(funcs.TASK_COUNT):
+        process = PythonOperator(
+            task_id=f"process_number_{index}",
+            python_callable=funcs.process_number,
+            op_kwargs={
+                "index": index,
+                "numbers": XCOM_PULL % generate.task_id,
+            },
+            retries=0,
+        )
+        generate >> process >> gather
+
+    report = PythonOperator(
+        task_id="report_numbers",
+        python_callable=funcs.report_numbers,
+        provide_context=True,
+    )
+
+    get_seed >> determine_continue >> [workflow_stopped, generate]
+    gather >> report
---
+++ airflow-1.10.9/dags/funcs.py
@@ -0,0 +1,47 @@
+import logging
+import random
+import time
+from ast import literal_eval as _
+
+from airflow.models import Variable
+
+
+TASK_COUNT = 5
+
+log = logging.getLogger(__name__)
+
+
+def determine_seed(conf):
+    # First get from Variables
+    seed = Variable.get("seed", deserialize_json=True, default_var=0)
+    # Override with context if provided, defaulting to current time if not
+    seed = _(conf).get("seed", seed or int(time.time()))
+
+    return seed
+
+
+def should_continue(seed):
+    if _(seed) % 2 == 0:
+        return "do_not_continue"
+    return "generate_numbers"
+
+
+def generate_numbers(seed):
+    random.seed(_(seed))
+    numbers = [random.randrange(100) for _ in range(TASK_COUNT)]
+    return numbers
+
+
+def process_number(index, numbers):
+    number = _(numbers)[index]
+    if number % 3 == 0:
+        raise ValueError(f"Cannot process value: {number}")
+    return number**2
+
+
+def report_numbers(**context):
+    total = 0
+    for index in range(TASK_COUNT):
+        number = context["ti"].xcom_pull(f"process_number_{index}") or 0
+        total += number
+    print(f"Total value computed: {total}")
